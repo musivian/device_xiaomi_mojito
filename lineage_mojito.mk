796be7f64410ba1592c62d40b2eb94916c34e128
musivian <kizura.vian@gmail.com>
2024-12-02 04:14:41 +0800

    mojito: bringup risingos

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -12,26 +12,35 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
 # Inherit some LineageOS stuffs
-$(call inherit-product, vendor/everest/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Maintainer name for Everest
-EVEREST_BUILD_TYPE := OFFICIAL
-EVEREST_MAINTAINER := K4LCHAKRA_&_ROMGHARTI
-
-# Build GAPPS\Vanilla
-WITH_GAPPS := true
-
-# Quick witch (add more than one Launcher in build)
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_CORE_GMS := true
+WITH_GMS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := everest_mojito
+PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 10
+PRODUCT_MODEL := M2101K7AG
 PRODUCT_MANUFACTURER := Xiaomi
 
+# List of add-ons
+PRODUCT_PACKAGES += \
+    MarkupGoogle \
+    LatinIMEGooglePrebuilt \
+    AiWallpapers \
+    WallpaperEmojiPrebuilt \
+    PrebuiltDeskClockGoogle \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingMaintainer="ZNAIV" \
+    RisingChipset="Qualcomm SDM678"
+	
